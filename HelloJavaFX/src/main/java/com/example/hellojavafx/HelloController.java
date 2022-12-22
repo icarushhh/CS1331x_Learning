82880b30c4ff668fd1128f3b4f948d8a4e26fca0
icarus <icarushhh@mail.ustc.edu.cn>
2022-12-22 22:12:31 +0800

    final commit

--- HelloJavaFX/src/main/java/com/example/hellojavafx/HelloController.java
+++ HelloJavaFX/src/main/java/com/example/hellojavafx/HelloController.java
@@ -10,5 +10,6 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        System.out.println("Hello JavaFX");
     }
 }
